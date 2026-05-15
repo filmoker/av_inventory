e497e3e6ab6609d565f0aa1409ab4d41b8e995ce
filmoker <filmoppo2@gmail.com>
2026-05-15 16:16:22 +0700

    อัพเดทหน้าปุ่มหน้า Index และ เพิ่มปุ่มหน่วยงานในหน้า Index

--- index.php
+++ index.php
@@ -139,6 +139,7 @@ $broken_link = "equipments.php?filter=" . urlencode("ชำรุด") . ($base_
                 
                 <a href="locations.php"><i class="fas fa-map-marker-alt me-2"></i> จัดการสถานที่</a>
                 <a href="categories.php"><i class="fas fa-tags me-2"></i> จัดการหมวดหมู่</a>
+                <a href="units.php"><i class="fas fa-layer-group me-2"></i> จัดการหน่วยงาน</a>
                 <a href="report.php"><i class="fas fa-print me-2"></i> พิมพ์รายงานสรุปยอด</a>
                 <a href="logout.php"><i class="fas fa-sign-out-alt me-2"></i> ออกจากระบบ</a>
             </nav>
