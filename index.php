0551b70cbf1ad7dd5d268484dc0ea2a255de93dd
filmoker <filmoppo2@gmail.com>
2026-05-19 10:49:05 +0700

    เปลี่ยนสีประสานมิตรให้เป็นสีแดงในหน้า Index.php

--- index.php
+++ index.php
@@ -173,7 +173,7 @@ $broken_link = "equipments.php?filter=" . urlencode("ชำรุด") . ($base_
                     </a>
                     
                     <div class="vr mx-1 d-none d-md-block"></div> <div class="btn-group">
-                        <a href="index.php?location=ประสานมิตร" class="btn <?php echo ($current_loc == 'ประสานมิตร') ? 'btn-primary' : 'btn-outline-primary'; ?> fw-bold px-4">ประสานมิตร</a>
+                        <a href="index.php?location=ประสานมิตร" class="btn <?php echo ($current_loc == 'ประสานมิตร') ? 'btn-danger' : 'btn-outline-danger'; ?> fw-bold px-4">ประสานมิตร</a>
                         <a href="index.php?location=องครักษ์" class="btn <?php echo ($current_loc == 'องครักษ์') ? 'btn-primary' : 'btn-outline-primary'; ?> fw-bold px-4">องครักษ์</a>
                     </div>
 
